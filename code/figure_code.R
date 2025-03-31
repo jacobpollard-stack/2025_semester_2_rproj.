79c6969d1239e87920ba90c16a7647d5fa402f38
Jacob Pollard <jacobpollard2915@gmail.com>
2025-03-31 16:25:13 +0100

    Stats tests done and overlay plotted

--- code/figure_code.R
+++ code/figure_code.R
@@ -2,7 +2,8 @@
 library(tidyverse)
 library(ggplot2)
 library(readxl)
-library(dlpyr)
+library(FSA)
+library(ggstatsplot)
 
 # Read in the data sheetwise and omit missing data.
 ## Define the file path.
@@ -10,7 +11,8 @@ path <- 'data/data_tidy_sheeted.xlsx'
 ## Load the sheet names into a variable.
 sheets <- excel_sheets(path)
 ## Read the sheets and compile them into one dataframe.
-data_tidy_list <- lapply(sheets, function(sheet) {
+data_tidy_list <- lapply(sheets, 
+                         function(sheet) {
   data <- read_excel(path, sheet = sheet)
   data$group <- sheet
   return(data)
@@ -25,20 +27,123 @@ data_summary <- data_tidy |>
   summarise(mean = mean(cfu_count_undiluted),
             sd = sd(cfu_count_undiluted),
             n = length(plate),
-            se = sd/sqrt(n)
+            se = sd / sqrt(n)
             )
 
-# Statistical tests
-## Check for normality
+# Statistical tests.
+## Check for normality in each sheet.
 mod <- lm(cfu_count_undiluted ~ group, data = data_tidy)
 ### Freedman-Diaconis rule for bin width.
-ggplot(mapping = aes(x = mod$residuals)) +
-  geom_histogram(bins = 15)
+iqr <- IQR(data_tidy$cfu_count_undiluted)
+n <- nrow(data_tidy)
+bin_width <- 2 * iqr / (n^(1/3))
+### Plot residuals to visually check for normality.
+ggplot(mapping = aes(x = mod$residuals),
+       data = data_tidy
+       ) +
+  geom_histogram(binwidth = bin_width)
 ### Data appears to have a positive skew.
 shapiro.test(data_tidy$cfu_count_undiluted)
-### p = 3.579e-05 < 0.05, there is evidence to suggest that the data is not normally distributed.
+### p = 1.743e-8 < 0.05, there is evidence to suggest that the data is not normally distributed.
 ## Therefore we will perform a non-parametric test, eg. the Kruskal-Wallace test.
 kruskal.test(cfu_count_undiluted ~ plate, data = data_tidy)
-### p = 0.7832 > there is no evidence to suggest that the data is significantly different between plates.
+### p = 0.7391 > 0.05 therefore there is no evidence to suggest that moisture level nor presence of hexadecane impacts growth of P. putida.
 
-# However, this is using the data of the mean of each CFU count between groups; we can instead analyse each group individually.
+# Visualise data.
+ggplot() +
+  geom_point(data = data_tidy,
+             mapping = aes(x = plate, 
+                           y = cfu_count_undiluted, 
+                           colour = group)
+             ) +
+  geom_point(data = data_summary, 
+             mapping = aes(x = plate, 
+                           y = mean), 
+             colour = 'black', 
+             size = 1
+             ) +
+  geom_errorbar(data = data_summary, 
+                mapping = aes(x = plate, 
+                              ymin = mean - se, 
+                              ymax = mean + se), 
+                width = 0.2, 
+                colour = 'black'
+                ) +
+  labs(title = 'CFU Count per Plate by Group and Mean with Error Bars',
+       x = 'Plate',
+       y = 'CFU Count',
+       colour = 'Group'
+       ) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_minimal()
+### The standard deviations are very high for each group, suggesting that the insignificance is due largely to variation between groups.
+
+# However, as the standard deviations are very high between groups, we can perform tests on each group individually.
+## Data for 4 x 5microL spots was only collected for group 1.
+data_group1 <- read_excel('data/group_1_data.xlsx')
+
+# Summarise data.
+group1_summary <- data_group1 |> 
+  group_by(plate) |> 
+  summarise(mean = mean(cfu_count_undiluted),
+            sd = sd(cfu_count_undiluted),
+            n = length(cfu_count_undiluted),
+            se = sd / sqrt(n)
+            )
+# Check for normality in group 1's data.
+# Statistical tests.
+## Check for normality in each sheet.
+mod <- lm(cfu_count_undiluted ~ spot, data = data_group1)
+### Freedman-Diaconis rule for bin width.
+iqr <- IQR(data_group1$cfu_count_undiluted)
+n <- nrow(data_group1)
+bin_width <- 2 * iqr / (n^(1/3))
+### Plot residuals to visually check for normality.
+ggplot(mapping = aes(x = mod$residuals), data = data_group1) +
+  geom_histogram(binwidth = bin_width)
+### Data appears to have a positive skew.
+shapiro.test(data_tidy$cfu_count_undiluted)
+### p = 1.743e-8 < 0.05, there is evidence to suggest that the data is not normally distributed.
+## Therefore we will perform a non-parametric test, eg. the Kruskal-Wallace test.
+kruskal.test(cfu_count_undiluted ~ plate, data = data_group1)
+### p = 9.638e-5 < 0.05 therefore there is evidence to suggest that, in group 1's data, moisture level and presence of hexadecane impacts growth of P. putida.
+## We can perform a post-hoc Dunn test to determine which plates are significantly different
+dunnTest(data = data_group1, cfu_count_undiluted ~ plate, method = 'bonferroni')
+
+# Plotting the results of the Dunn test.
+ggstatsplot <- ggbetweenstats(
+  data = data_group1,
+  x = 'plate', 
+  y = 'cfu_count_undiluted', 
+  type = 'nonparametric', 
+  plot.title = "Dunn's Test Results",
+  comparison.group = 'all',
+  mean.point.args = list(size = 5, color = 'blue', shape = 16) # Adjust size and color here
+) +
+  labs(title = "Dunn's Test results for Group 1 data", x = 'Plate', y = 'Mean CFU count') +
+  theme(
+    plot.margin = margin(10, 10, 20, 10)
+  )
+
+# Overlaying linear regression onto our stats plot.
+## Grouping the plates with and without hexadecane into two categories, W and WO, respectively
+data_group1 <- data_group1 |> 
+  mutate(
+    group = ifelse(plate %in% c('30W', '40W', '50W', '60W'), 'W', 'WO')
+    )
+
+##Combining plots
+
+combined_plot <- ggstatsplot + 
+  geom_smooth(
+    data = data_group1,
+    formula = 'y ~ x',
+    aes(group = group, 
+        color = group), 
+    method = 'lm',
+  ) +
+  scale_color_manual(values = c('W' = 'blue', 'WO' = 'red'))
+
+## Print combined plot
+combined_plot
+

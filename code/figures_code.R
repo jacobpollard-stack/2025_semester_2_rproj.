39597313c8029a17f049709b90d383364b4ed3eb
Jacob Pollard <jacobpollard2915@gmail.com>
2025-04-01 13:44:02 +0100

    Group 3 plot almost finished

--- code/figures_code.R
+++ code/figures_code.R
@@ -157,7 +157,7 @@ theme_custom <- theme(
     plot.title = element_text(hjust = 0,
                               vjust = -5),
     plot.margin = margin(10, 10, 40, 10),
-    plot.subtitle = element_text(vjust = -260,
+    plot.subtitle = element_text(vjust = -250,
                                  hjust = 1)
 )
 
@@ -176,7 +176,7 @@ combined_plot <- ggstatsplot +
   scale_colour_manual(values = c('With hexadecane' = 'blue', 
                                  'Without hexadecane' = 'red')
   ) +
-  labs(title = "Dunn's Test results for Group 1 data", 
+  labs(title = "Group 1 CFU count per µL at varying moisture levels, \n with and without hexadecane contamination", 
        x = 'Soil moisture level /% of field capacity', 
        y = 'Mean CFU count /µL') +
   cowplot::theme_cowplot() + theme_custom
